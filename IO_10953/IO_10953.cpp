4812d2469fca731f403d93be275705152a1108e2
VOCA <lcw0115a@gmail.com>
2023-04-21 21:51:32 +0900

    230421

---
+++ IO_10953/IO_10953.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int T;
+	int a, b;
+	char comma;
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> a >> comma >> b;
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
