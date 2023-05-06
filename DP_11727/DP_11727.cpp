d2387439cb1a7580687b9a8b3ba8046b2c1131bb
VOCA <lcw0115a@gmail.com>
2023-05-06 19:43:09 +0900

    230506

--- DP_11727/DP_11727.cpp
+++ DP_11727/DP_11727.cpp
@@ -7,18 +7,13 @@ int main() {
 	int N;
 	cin >> N;
 
-	int mem[12] = { 0, };
+	vector<int> mem(N + 1, 0);
 	mem[1] = 1;
-	mem[2] = 2;
-	mem[3] = 4;
+	mem[2] = 3;
 
-	for (int i = 0; i < N; i++) {
-		int M;
-		cin >> M;
-
-		for (int j = 4; j <= M; j++) {
-			mem[j] = mem[j - 1] + mem[j - 2] + mem[j - 3];
-		}
-		cout << mem[M] << "\n";
+	for (int i = 3; i <= N; i++) {
+		mem[i] = (mem[i - 1] + mem[i - 2] * 2) % 10007;
 	}
+
+	cout << mem[N];
 }
